6a6d57763aefd6b91dd8c40b6663ede171f2e96c
SafalKoirala <safalkoirala92@gmail.com>
2021-05-18 21:18:20 +0545

    fragments created for all navigation items

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -1,38 +1,71 @@
 package com.example.myapplication;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.view.View;
 
+import com.example.myapplication.Fragments.BudgetFragment;
+import com.example.myapplication.Fragments.HomeFragment;
+import com.example.myapplication.Fragments.MapFragment;
+import com.example.myapplication.Fragments.ProfileFragment;
 import com.example.myapplication.databinding.ActivityDashboardBinding;
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    BottomNavigationView bnv;
+
 
     ActivityDashboardBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.logout.setOnClickListener(new View.OnClickListener() {
+//
+        //fragments work
+        getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer,new HomeFragment()).commit();
+        bnv = (BottomNavigationView)findViewById(R.id.bottomNavigation);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                auth.signOut();
-                Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                Fragment temp=null;
+                switch(item.getItemId()){
+                    case R.id.home:
+                        temp= new HomeFragment();
+                        break;
+                    case R.id.budget:
+                        temp= new BudgetFragment();
+                        break;
+                    case R.id.map:
+                        temp= new MapFragment();
+                        break;
+                    case R.id.profile:
+                        temp= new ProfileFragment();
+                        break;
+                }
+                //Replacing the FrameContainer with our temp container
+                getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer,temp).commit();
+                return true;
             }
         });
 
 
-
-
     }
+
+
 }
+
 //TODO: add fragments for each menu items and design those fragments
---
+++ app/src/main/java/com/example/myapplication/Fragments/ProfileFragment.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.myapplication.LoginActivity;
+import com.example.myapplication.R;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+    //so many unnecessary things are given while creating fragments so removed it
+    //variables are declared here in fragment
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    TextView logout;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        logout = view.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        return view;
+    }
+}
