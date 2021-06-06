a90702d63d9c39b2bfaa797e40cc5230b5beecc6
SafalKoirala <safalkoirala92@gmail.com>
2021-06-06 21:16:06 +0545

    trip addition layout and modals created

--- app/src/main/java/com/example/myapplication/Fragments/BudgetFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/BudgetFragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication.Fragments;
 
 import android.os.Bundle;
 import android.content.DialogInterface;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -25,24 +26,27 @@ public class BudgetFragment extends Fragment {
     public BudgetFragment() {
 
     }
+
     private Button addPlan;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         View view = inflater.inflate(R.layout.fragment_budget, container, false);
+        View view = inflater.inflate(R.layout.fragment_budget, container, false);
         addPlan = view.findViewById(R.id.btnAddPlan);
 
-//        addPlan.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                PlanFragment planFragment = new PlanFragment();
-//                if(getFragmentManager() != null) {
-//                    planFragment.show(getFragmentManager(), "ADD a trip");
-//                }
-//            }
-//        });
+        addPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                if (fm != null) {
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.replace(R.id.FrameContainer, new PlanFragment());
+                    ft.commit();
+                }
+            }
+        });
 
 
         return view;
--- app/src/main/java/com/example/myapplication/Fragments/PlanFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/PlanFragment.java
@@ -8,6 +8,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,31 +19,198 @@ import android.widget.Toast;
 
 import com.example.myapplication.Models.Image;
 import com.example.myapplication.R;
+import com.example.myapplication.RegistrationActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
+import static com.example.myapplication.MainActivity.MY_DATABASE;
 
 public class PlanFragment extends Fragment {
 
     public PlanFragment() {
         // Required empty public constructor
     }
-private EditText tripName,Amount,startDate,endDate;
+
+    private EditText tripName;
+    private EditText Amount;
+    private EditText startDate;
+    private EditText endDate;
     private Button setTrip;
+    //Firebase Classes
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseDatabase database;
+    private final DatabaseReference databaseReference = FirebaseDatabase.getInstance(MY_DATABASE).getReference();
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference("trips");
 
 
-    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_plan, null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_plan, container, false);
+        tripName = view.findViewById(R.id.tripName);
+        Amount = view.findViewById(R.id.amount);
+        startDate = view.findViewById(R.id.startDate);
+        endDate = view.findViewById(R.id.endDate);
+
+        setTrip = view.findViewById(R.id.setTrip);
+        //calendar input for date in start and end date text field
+        startDate.addTextChangedListener(new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
 
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                .setView(view).setTitle("Share Your Experience");
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8) {
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    } else {
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day = Integer.parseInt(clean.substring(0, 2));
+                        int mon = Integer.parseInt(clean.substring(2, 4));
+                        int year = Integer.parseInt(clean.substring(4, 8));
+
+                        if (mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon - 1);
+
+                        year = (year < 1900) ? 1900 : (year > 2100) ? 2100 : year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE)) ? cal.getActualMaximum(Calendar.DATE) : day;
+                        clean = String.format("%02d%02d%02d", day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    startDate.setText(current);
+                    startDate.setSelection(sel < current.length() ? sel : current.length());
 
 
+                }
+            }
 
-        return builder.create();
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        endDate.addTextChangedListener(new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8) {
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    } else {
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day = Integer.parseInt(clean.substring(0, 2));
+                        int mon = Integer.parseInt(clean.substring(2, 4));
+                        int year = Integer.parseInt(clean.substring(4, 8));
+
+                        if (mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon - 1);
+
+                        year = (year < 1900) ? 1900 : (year > 2100) ? 2100 : year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE)) ? cal.getActualMaximum(Calendar.DATE) : day;
+                        clean = String.format("%02d%02d%02d", day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    endDate.setText(current);
+                    endDate.setSelection(sel < current.length() ? sel : current.length());
+
+
+                }
+            }
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
+        setTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tripname = tripName.getText().toString();
+                String amount = Amount.getText().toString();
+                String startdate = startDate.getText().toString();
+                String enddate = endDate.getText().toString();
+                if (tripName != null && Amount != null && startDate != null && endDate != null) {
+                    addToFirebase(tripname, amount, startdate, enddate);
+                }
+
+            }
+        });
+
+        return view;
     }
 
+    private void addToFirebase(String tripName, String Amount, String startDate, String endDate) {
+      //  String tripId = databaseReference.push().getKey();
+
+
+    }
 
 }
